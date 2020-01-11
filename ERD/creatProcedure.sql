cc76d7986b38877d245140d475681ca4ae637d3d
hphpt <hphpt@DESKTOP-I56L6VA>
2020-01-12 01:49:56 +0900

    database 관련 커밋 : 김후영

---
+++ ERD/creatProcedure.sql
@@ -0,0 +1,61 @@
+
+
+-- 1. 1번 쿼리만 먼저 실행한 후에 dbeaver 환경으로 전환한 후 
+-- 	    현재 연결된 db -> Databases -> mydb -> Procedures 
+-- 	  -> get_uid_by_location 클릭 -> source 부분에 가셔서 2번 쿼리 전부 복붙하고 저장까지 해주시면 완료입니다
+
+-- 1번
+DROP PROCEDURE IF EXISTS get_uid_by_location;
+CREATE PROCEDURE get_uid_by_location
+(
+	in on_latitude double,
+	in on_longitude double
+)
+begin
+	declare cnt int;
+end;
+
+
+
+/* 2번
+CREATE DEFINER=`myuser`@`localhost` PROCEDURE `mydb`.`get_uid_by_location`(
+	in on_latitude double,
+	in on_longitude double
+)
+begin
+	(SELECT dist.t_uid FROM(
+		SELECT t.t_uid "t_uid", 
+				( 6371 * acos( cos( radians(on_latitude) )
+				* cos( radians(t.t_latitude) )
+				* cos( radians( t.t_longitude)
+				- radians(on_longitude) )
+				+ sin( radians(on_latitude) )
+			* sin( radians(t.t_latitude) ) ) ) AS distance
+		FROM toilet_info t) dist
+	WHERE dist.distance < 0.5);
+END
+*/
+
+
+
+
+
+
+-- -------------
+/*
+-- 이 함수는 실행 하셔도 되고 안하셔도 됩니다
+DROP PROCEDURE IF EXISTS clac_location;
+CREATE PROCEDURE calc_location
+(
+	in on_latitude double,
+	in on_longitude double
+)
+begin
+END;
+*/
+
+-- call calc_location(37.509976, 127.020663, @cnt);
+-- call get_uid_by_location(37.509976, 127.020663);
+
+-- 화장실 정보 갯수 
+-- select count(*) from toilet_info;
