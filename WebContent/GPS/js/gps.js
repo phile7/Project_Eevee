b8d485cda5340d6ae12776f5ae289dfcd648848c
phile <phile@LAPTOP-3NS2T0J9>
2020-01-13 20:43:19 +0900

    0113/병채복구1

---
+++ WebContent/GPS/js/gps.js
@@ -0,0 +1,14 @@
+
+
+$("#btn1").click(function(){
+    $("#btn_bar").animate({"left" : "0px"})
+});
+    
+$("#btn_close").click(function(){
+    $("#btn_bar").animate({"left" : "-300px"})
+});
+
+$("#pop").click(function(){
+    $("#pop").animate({"bottom" : "-600px"})
+});
+
